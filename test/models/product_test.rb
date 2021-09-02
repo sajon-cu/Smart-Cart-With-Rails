103f48e86b52836b3f4dd5c43ec5af7bf39684dd
Sajoncu <saifulislamsajon@gmail.com>
2021-09-02 16:22:05 +0600

    change the layout

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors["title"].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(
+      title: "The best adventure",
+      description: 'xyz',
+      image_url: 'xyz.png',
+      price: -1
+    )
+
+    assert product.invalid?
+
+    assert_equal ['must be greater than or equal to 0.01'], product.errors[:price]
+
+    product.price = 1
+    assert product.invalid?
+
+    assert_equal([], product.errors[:price])
+
+    product.price = 1
+    assert product.valid?
+
+  end
 end
